2c76345e46496517bc67d999a6e4b278cd3ffb54
DiegoCedeno12 <dcedeno7300@utm.edu.ec>
2022-07-10 11:15:41 -0500

    Proyecto añadido Facturacion

--- Facturacion/core/views.py
+++ Facturacion/core/views.py
@@ -4,11 +4,23 @@ from django.shortcuts import redirect, render, HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import Contact, ContactForm
+from producto.models import registrar
+from producto.forms import ProductoForm
 
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
 
+def registrarprod(request):
+    data = { 'form': ProductoForm() }
+    if request.method == 'POST':
+        formulario = ProductoForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data["mensaje"] = "Registro Enviado"
+        else:
+            data["form"] = formulario
+    return render(request, "producto/registrarprod.html")
 
 def contact(request):
     data = { 'form': ContactForm() }
